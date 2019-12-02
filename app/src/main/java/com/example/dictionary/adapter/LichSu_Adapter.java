9c5ca33b2c2ebdf25212aaba6120b95214508567
truongto <totruong52@gmail.com>
2019-12-02 09:12:44 +0700

    update_3

--- app/src/main/java/com/example/dictionary/adapter/LichSu_Adapter.java
+++ app/src/main/java/com/example/dictionary/adapter/LichSu_Adapter.java
@@ -1,4 +1,31 @@
 package com.example.dictionary.adapter;
 
-public class LichSu_Adapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class LichSu_Adapter extends RecyclerView.Adapter<LichSu_Adapter.Holder> {
+    @NonNull
+    @Override
+    public LichSu_Adapter.Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LichSu_Adapter.Holder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class Holder extends RecyclerView.ViewHolder {
+        public Holder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
